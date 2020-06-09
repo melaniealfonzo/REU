990140e9b82648c9bddf57c2b3fdea8ad758f415
melaniealfonzo <62857538+melaniealfonzo@users.noreply.github.com>
2020-06-09 13:22:35 -0400

    Create outcomes_covid.py

---
+++ outcomes_covid.py
@@ -0,0 +1,6 @@
+o = ['severe','died','discharged','alive','other']
+
+d_outcome = {nan : 0, 'discharged' : 'discharged', 'discharge': 'dishcharged', 'died':'died', 'Discharged':'discharged', '05.02.2020' :'other', 'death': 'died',
+'Symptoms only improved with cough. Currently hospitalized for follow-up.' : 'hospitalized',
+'recovered' :'alive', 'stable':'alive', 'critical condition, intubated as of 14.02.2020' : 'severe',
+'treated in an intensive care unit (14.02.2020)' :'severe', 'severe' :'severe'}
