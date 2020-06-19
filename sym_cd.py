64695531d0d150a0a6c040c5d8bd7f48ec9a81c1
melaniealfonzo <62857538+melaniealfonzo@users.noreply.github.com>
2020-06-18 21:01:26 -0400

    Create sym_cd.py

---
+++ sym_cd.py
@@ -0,0 +1,65 @@
+symp = {'cough':'respiratory', 'fatigue':'weakness/pain', 'fever':'fever', 'sputum':'respiratory', 'myalgias':'weakness/pain', 'shortness of breath':'respiratory',
+ 'respiratory symptoms':'respiratory', 'chest pain':'weakness/pain', 'diarrhea':'gastrointestinal', 'rhinorrhea':'respiratory', 'sneezing':'respiratory',
+ 'sore throat':'other', 'gasp':'other', 'weakness/pain':'weakness/pain', 'chills':'weakness/pain', 'headache':'nausea', 'somnolence':'weakness/pain',
+ 'vomiting':'gastrointestinal', 'pleural effusion':'chest', 'dizziness':'nausea', 'pneumonitis':'respiratory', 'soreness':'weakness/pain',
+ 'pneumonia':'respiratory', 'joint pain':'weakness/pain', 'nausea':'nausea', 'body soreness':'weakness/pain', 'cold symptoms':'respiratory',
+ 'pharyngalgia':'respiratory', 'malaise':'weakness/pain', 'other symptoms':'other', 'systemic weakness/pain':'weakness/pain',
+ 'conjunctivitis':'other', 'sore muscle':'weakness/pain', 'lack of energy':'weakness/pain', 'dry mouth':'other', 'dry throat':'other',
+ 'anorexia':'weakness/pain', 'flu-like symptoms':'respiratory', 'sore limbs':'weakness/pain', 'sweating':'other', 'abdominal pain':'weakness/pain',
+ 'discharge':'other', 'acute left heart failure':'cardiac', 'acute coronary syndrome':'cardiac',
+ 'Quantity[37.' 'DegreesCelsius]':'fever', 'acute pharyngitis':'respiratory', 'Quantity[37.1':'fever',
+ 'Quantity[38.2':'fever', 'poor physical condition':'weakness/pain', 'toothache':'other', 'Quantity[37.7':'fever',
+ 'Quantity[37.5':'fever', 'Quantity[38.3':'fever', 'Quantity[38.':'fever', 'Quantity[39.':'high fever',
+ 'Quantity[39.3':'high fever', 'influenza':'flu', 'inappetence':'other', 'Quantity[38.7':'fever', 'rash':'other',
+ 'Quantity[Interval[38':'fever', '39]':'high fever', 'acute respiratory distress syndrome':'respiratory',
+ 'Quantity[Interval[37':'fever', 'Quantity[40.':'high fevere', 'Quantity[37.8':'fevere', 'Quantity[37.2':'fever',
+ 'difficulty walking':'weakness/pain', 'back pain':'weakness/pain', 'Quantity[38.1':'fever', 'breathing difficulty':'respiratory',
+ 'acute respiratory failure':'respiratory', 'rhinitis':'respiratory', 'mild symptoms':'other',
+ 'multiple organ failure':'other', 'dysphagia':'weakness/pain', 'septic shock':'cardiac' ,'cardiogenic shock':'cardiac',
+ 'acute renal failure':'kidney', 'asymptomatic':'asymptomatic', 'catarrh':'respiratory',
+ 'COVID-19 related symptoms':'respiratory', 'respiratory failure':'respiratory',
+ 'no respiratory symptoms':'asymptomatic', 'esophageal reflux':'gastrointestinal',
+ 'similar to a respiratory infection':'respiratory', 'acute respiratory viral infection':'respiratory',
+ 'afebrile':'asymptomatic', 'kidney failure':'kidney', 'hypertension':'cardiac', 'digestive discomfort':'gastrointestinal',
+ 'aggressive pulmonary symptomatology':'respiratory', 'lesions on chest radiographs':'respiratory',
+ 'acute respiratory disease syndrome':'respiratory', 'wheezing':'repsiratory', 'full body slump':'weakness/pain',
+ 'primary myelofibrosis':'other', 'cardiac arrhythmia':'cardiac', 'heart failure':'cardiac',
+ 'myocardial infarction':'cardiac', 'multiple electrolyte imbalance':'other',
+ 'acute respiratory disease':'respiratory', 'acute myocardial infarction':'cardiac',
+ 'congestive heart failure':'cardiac', 'acute respiratory infection':'respiratory', 'sepsis':'cardiac',
+ 'acute kidney injury':'kidney', 'arrhythmia' :'cardiac', 'myocardial dysfunction':'cardiac', 'gastritis':'gastrointestinal',
+ 'cardiopulmonary arrest':'cardiac', 'severe acute respiratory distress syndrome':'respiratory',
+ 'hypoxia':'respiratory'}
+
+
+
+symtpoms = ['respiratory','weakness/pain','fever','gastrointestinal','other','nausea','cardiac','high fever','kidney','asymptomatic',]
+
+
+
+
+
+cd = {'diabetes':'diabetes', 'cerebral infarction':'neuro', 'Missing[NotAvailable]':'NA', 'hypertension':'hypertension',
+ 'colon cancer':'cancer', "Parkinson's disease":'neuro', 'encephalomalacia':'neuro', 'hip replacement':'ortho',
+ 'chronic bronchitis':'repsiratory', 'stenocardia':'cardiac', 'coronary stent':'cardiac',
+ 'hemorrhage of digestive tract':'gastrointestinal', 'chronic obstructive pulmonary disease':'respiratory',
+ 'chronic renal insufficiency':'kidney', 'coronary heart disease':'cardiac', 'tuberculosis':'respiratory',
+ 'frequent ventricular premature beat':'cardiac', 'hypertriglyceridemia':'blood', 'asthma':'respiratory',
+ 'prostate hypertrophy':'prostate', 'hepatitis B':'kidney', 'type 2 diabetes':'diabetes',
+ 'coronary bypass surgery':'cardiac', 'kidney disease':'kidney', 'on dialysis':'kidney',
+ 'chronic kidney disease':'kidney', 'valvular heart disease':'cardiac',
+ 'upper gastrointestinal bleeding':'gastrointestinal', 'prerenal azotemia':'blood', 'cardiomyopathy':'cardiac',
+ 'benign prostatic hyperplasia':'prostate', 'bronchial asthma':'respiratory',
+ 'impaired fasting glucose':'blood', 'dyslipidemia':'blood', 'atherosclerosis':'ortho',
+ 'atrial fibrillation':'cardiac', 'cerebrovascular infarct':'neuro', 'cardiac disease':'cardiac',
+ 'hypothyroidism':'thyroid', 'cardiovascular disease':'cardiac', 'prostate cancer':'prostate',
+ 'tongue cancer':'cancer', 'benign prostatic hypertrophy':'prostate', 'cardiac dysrhythmia':'cardiac',
+ 'hyperthyroidism':'thyroid', 'myocarditis':'cardiac', 'cerebrovascular accident infarct':'cardiac'}
+
+
+chronic_diseases = ['diabetes', 'neuro','NA','hypertension','cancer','ortho','respiratory','cardiac','gastrointestinal','kidney','blood','prostate','thyroid',]
+
+
+
+
+
