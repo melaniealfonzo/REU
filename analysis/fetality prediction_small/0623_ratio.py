d74b324a09af93db5a46eb096962ffe21714e7af
quintinali <tina.yuner@gmail.com>
2020-07-02 12:26:44 -0700

    analysis on small dataset

--- analysis/fetality prediction_small/0623_ratio.py
+++ analysis/fetality prediction_small/0623_ratio.py
@@ -1,14 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jun 25 14:58:56 2020
-
-@author: MelanieAlfonzo
-"""
-
-import csv
 import pandas as pd 
-import datetime
 import numpy as np
 import seaborn
 import matplotlib.pyplot as plt
@@ -22,7 +12,7 @@ AgeRange_Dict = {
       "40-49": 5,
       "50-59": 6,
       "60-69": 7,
-      #"70-79": 8,
+      "70-79": 8,
       "80-89": 9,
       "90-99": 10
     }
@@ -42,77 +32,7 @@ def correlation_analysis(df):
     plt.yticks(rotation=0) 
     plt.xticks(rotation=90)
     plt.show()
-    
-def perpareTrainData(df):
-    '''
-    df['DateOfOnsetSymptoms']= pd.to_datetime(df['DateOfOnsetSymptoms']) 
-    select_day = datetime.datetime(2020, 5, 29) 
-    train_df = df.loc[(df.DateOfOnsetSymptoms <= select_day)]
-    test_df = df.loc[(df.DateOfOnsetSymptoms > select_day)]
-    '''
-    
-    #df['DateOfOnsetSymptoms']= pd.to_datetime(df['DateOfOnsetSymptoms']) 
-    #select_day = datetime.datetime(2020, 2, 26) 
-    #non_test_df = df.loc[(df.DateOfOnsetSymptoms <= select_day)]
-    #test_df = df.loc[(df.DateOfOnsetSymptoms > select_day)]
-    
-    #select_day2 = datetime.datetime(2020, 2, 13) 
-    #train_df = non_test_df.loc[(non_test_df.DateOfOnsetSymptoms <= select_day2)]
-    #valid_df = non_test_df.loc[(non_test_df.DateOfOnsetSymptoms > select_day2)]
-    
-    patient_file = "small_records_0623.csv"
-    df = pd.read_csv(patient_file)
 
-    df_d = df[df['death_binary'] == 1]
-    df_a = df[df['death_binary'] == 0]
-
-
-    df_d.to_csv('df_d.csv')
-    df_a.to_csv('df_a.csv')
-
-
-    df_a_nontest = df_a[:1013]
-    df_a_test = df_a[1013:]
-
-    df_d_nontest = df_d[:86]
-    df_d_test = df_d[86:]
-
-    nontest_df = pd.concat([df_a_nontest, df_d_nontest], axis=0)
-    test_df = pd.concat([df_a_test, df_d_test], axis=0)
-
-    df_a_train = df_a_nontest[:709]
-    df_a_valid = df_a_nontest[709:]
-
-    df_d_train = df_d_nontest[:60]
-    df_d_valid = df_d_nontest[60:]
-
-    train_df = pd.concat([df_a_train, df_d_train], axis=0)
-    valid_df = pd.concat([df_a_valid, df_d_valid], axis=0)
-    
-    print(train_df.shape)
-    print(valid_df.shape)
-    print(test_df.shape)
-    
-    
-    if data_process == 1:
-        x_train_df =  train_df[['AgeRange_code','gender_binary','respiratory','weakness/pain','fever','gastrointestinal','other','nausea','cardiac','high fever','kidney','asymptomatic','diabetes','neuro','NA','hypertension','cancer','ortho','respiratory_CD','cardiacs_cd','kidney_CD','blood','prostate','thyroid']]
-        x_test_df =  test_df[['AgeRange_code','gender_binary','respiratory','weakness/pain','fever','gastrointestinal','other','nausea','cardiac','high fever','kidney','asymptomatic','diabetes','neuro','NA','hypertension','cancer','ortho','respiratory_CD','cardiacs_cd','kidney_CD','blood','prostate','thyroid']]
-    else:
-        x_train_df =  train_df[['age','gender_binary','respiratory','weakness/pain','fever','gastrointestinal','other','nausea','cardiac','high fever','kidney','asymptomatic','diabetes','neuro','NA','hypertension','cancer','ortho','respiratory_CD','cardiacs_cd','kidney_CD','blood','prostate','thyroid']]
-        x_test_df =  test_df[['age','gender_binary','respiratory','weakness/pain','fever','gastrointestinal','other','nausea','cardiac','high fever','kidney','asymptomatic','diabetes','neuro','NA','hypertension','cancer','ortho','respiratory_CD','cardiacs_cd','kidney_CD','blood','prostate','thyroid']]
-
-    y_train_df =  train_df[['death_binary']]
-    y_test_df = test_df[['death_binary']]
-    
-    death_rate_train = (y_train_df != 0).values.sum() / len(y_train_df)
-    death_rate_test = (y_test_df != 0).values.sum() / len(y_test_df)
-    
-    x_train = x_train_df.values
-    x_test = x_test_df.values
-    y_train = y_train_df.values.flatten()
-    y_test = y_test_df.values.flatten()
-    
-    return x_train, x_test, y_train, y_test
      
 def is_int_number(data):
     try:
@@ -165,10 +85,73 @@ def preprocessData_2(df):
     #convert age to age_range
     age_number_df = df.loc[df['age_is_number'] == 1]
     return age_number_df
- 
+    
+def perpareTrainData(df):
+    
+    df_d = df[df['death_binary'] == 1]
+    df_a = df[df['death_binary'] == 0]
+    
+    len_d = len(df_d.index)
+    len_a = len(df_a.index)
+    
+    test_pos_d =  int(len_d*0.7)
+    test_pos_a =  int(len_a*0.7)
+    
+    
+    df_a_nontest = df_a[:test_pos_a]
+    df_a_test = df_a[test_pos_a:]
+    
+    df_d_nontest = df_d[:test_pos_d]
+    df_d_test = df_d[test_pos_d:]
+    
+    test_df = pd.concat([df_a_test, df_d_test], axis=0)
+    
+    
+    valid_pos_d =  int(test_pos_d*0.7)
+    valid_pos_a =  int(test_pos_a*0.7)
+    
+    df_a_train = df_a_nontest[:valid_pos_a]
+    df_a_valid = df_a_nontest[valid_pos_a:]
+    
+    df_d_train = df_d_nontest[:valid_pos_d]
+    df_d_valid = df_d_nontest[valid_pos_d:]
+    
+    train_df = pd.concat([df_a_train, df_d_train], axis=0)
+    valid_df = pd.concat([df_a_valid, df_d_valid], axis=0)
+    
+    
+    print(train_df.shape)
+    print(valid_df.shape)
+    print(test_df.shape)
+    
+    
+    if data_process == 1:
+        x_train_df =  train_df[['AgeRange_code','gender_binary','respiratory','weakness/pain','fever','gastrointestinal','other','nausea','cardiac','high fever','kidney','asymptomatic','diabetes','neuro','NA','hypertension','cancer','ortho','respiratory_CD','cardiacs_cd','kidney_CD','blood','prostate','thyroid']]
+        x_test_df =  test_df[['AgeRange_code','gender_binary','respiratory','weakness/pain','fever','gastrointestinal','other','nausea','cardiac','high fever','kidney','asymptomatic','diabetes','neuro','NA','hypertension','cancer','ortho','respiratory_CD','cardiacs_cd','kidney_CD','blood','prostate','thyroid']]
+    else:
+        x_train_df =  train_df[['age','gender_binary','respiratory','weakness/pain','fever','gastrointestinal','other','nausea','cardiac','high fever','kidney','asymptomatic','diabetes','neuro','NA','hypertension','cancer','ortho','respiratory_CD','cardiacs_cd','kidney_CD','blood','prostate','thyroid']]
+        x_test_df =  test_df[['age','gender_binary','respiratory','weakness/pain','fever','gastrointestinal','other','nausea','cardiac','high fever','kidney','asymptomatic','diabetes','neuro','NA','hypertension','cancer','ortho','respiratory_CD','cardiacs_cd','kidney_CD','blood','prostate','thyroid']]
+
+    y_train_df =  train_df[['death_binary']]
+    y_test_df = test_df[['death_binary']]
+    
+    death_rate_train = (y_train_df != 0).values.sum() / len(y_train_df)
+    death_rate_test = (y_test_df != 0).values.sum() / len(y_test_df)
+    
+    print(death_rate_train)
+    #print(death_rate_train)
+    print(death_rate_test)
+    
+    x_train = x_train_df.values
+    x_test = x_test_df.values
+    y_train = y_train_df.values.flatten()
+    y_test = y_test_df.values.flatten()
+    
+    return x_train, x_test, y_train, y_test
+     
+
 patient_file = "small_records_0623.csv"
 df = pd.read_csv(patient_file)
-
 data_process = 1
 if data_process == 1:
     patient_df = preprocessData_1(df)
@@ -177,148 +160,111 @@ else:
     patient_df = preprocessData_2(df)
     patient_df = patient_df.drop(['age_is_number'], axis=1)
     
-
 #correlation_analysis(patient_df)
 x_train, x_test, y_train, y_test = perpareTrainData(patient_df)
 
-
 #logistic regression
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report, confusion_matrix
-model = LogisticRegression(solver='liblinear', random_state=0) 
-model.fit(x_train, y_train)
+from sklearn.metrics import confusion_matrix
+from sklearn import metrics
+from sklearn.metrics import roc_auc_score
+
 model = LogisticRegression(solver='liblinear', random_state=0).fit(x_train, y_train)
 predictions = model.predict(x_test)
-#get accuracy
-lr_score = model.score(x_test, y_test)
-print(lr_score)
-
-#use confusion matrix to get all 3
-from sklearn import metrics
-
-cnf_matrix = metrics.confusion_matrix(y_test, predictions)
+tn, fp, fn, tp = confusion_matrix(y_test, predictions).ravel()
+specificity = tn / (tn+fp)
+sensitivity  = tp / (fn + tp)
+roc_lr=roc_auc_score(y_test, predictions)
+lr_a = metrics.accuracy_score(y_test, predictions.round())
+print('')
 print('for logistic regression:')
 print("Accuracy:",metrics.accuracy_score(y_test, predictions))
 print("Precision:",metrics.precision_score(y_test, predictions))
 print("Recall:",metrics.recall_score(y_test, predictions))
-
-lr_a = metrics.accuracy_score(y_test, predictions)
-
-tn, fp, fn, tp = confusion_matrix(y_test, predictions).ravel()
-specificity = tn / (tn+fp)
+print("true negative:",tn)
+print("false postive:",fp)
+print("false negative:",fn)
+print("true postive:",tp)
 print('specificity', specificity)
-
-sensitivity  = tp / (fn + tp)
 print('sensitivity:', sensitivity)
-
-from sklearn.metrics import roc_auc_score
-roc_lr=roc_auc_score(y_test, predictions)
 print('AUC: ',roc_lr)
-
-tn, fp, fn, tp = confusion_matrix(y_test, predictions).ravel()
-specificity = tn / (tn+fp)
-print('specificity', specificity)
-
-sensitivity  = tp / (fn + tp)
-print('sensitivity:', sensitivity)
-
+print('********************************')
+print('')
 
 #SVM 
 from sklearn import svm
 clf = svm.SVC(kernel='linear')
 clf.fit(x_train, y_train)
 svm_pred = clf.predict(x_test)
-
-#use confusion matrix to get all 3
-from sklearn import metrics
-cnf_matrix = metrics.confusion_matrix(y_test, svm_pred)
-cnf_matrix
+tn, fp, fn, tp = confusion_matrix(y_test, svm_pred).ravel()
+s_specificity = tn / (tn+fp)
+s_sensitivity  = tp / (fn + tp)
+roc_svm=roc_auc_score(y_test, svm_pred)
+a_svm = metrics.accuracy_score(y_test, svm_pred.round())
 print("for SVM")
 print("Accuracy:",metrics.accuracy_score(y_test, svm_pred))
 print("Precision:",metrics.precision_score(y_test, svm_pred))
 print("Recall:",metrics.recall_score(y_test, svm_pred))
-
-
-a_svm = metrics.accuracy_score(y_test, svm_pred)
-tn, fp, fn, tp = confusion_matrix(y_test, svm_pred).ravel()
-s_specificity = tn / (tn+fp)
+print("true negative:",tn)
+print("false postive:",fp)
+print("false negative:",fn)
+print("true postive:",tp)
 print('specificity:', s_specificity)
-
-s_sensitivity  = tp / (fn + tp)
 print('sensitivity:', s_sensitivity)
-
-from sklearn.metrics import roc_auc_score
-roc_svm=roc_auc_score(y_test, svm_pred)
 print('AUC: ',roc_svm)
-
+print('********************************')
+print('')
 
 # random forest 
 from sklearn.ensemble import RandomForestRegressor
 rf = RandomForestRegressor(n_estimators = 1000, random_state = 42)
 rf.fit(x_train, y_train);
 rf_pred=rf.predict(x_test)
- 
+tn, fp, fn, tp = confusion_matrix(y_test, rf_pred.round()).ravel()
+rf_specificity = tn / (tn+fp)
+rf_sensitivity  = tp / (fn + tp)
+roc_rf=roc_auc_score(y_test, rf_pred.round())
+rf_a = metrics.accuracy_score(y_test, rf_pred.round())
 
-#use confusion matrix to get all 3
-from sklearn import metrics
-cnf_matrix = metrics.confusion_matrix(y_test, rf_pred.round())
 print('for random forrest:')
 print("Accuracy:",metrics.accuracy_score(y_test, rf_pred.round()))
 print("Precision:",metrics.precision_score(y_test, rf_pred.round()))
 print("Recall:",metrics.recall_score(y_test, rf_pred.round()))
-
-
-rf_a = metrics.accuracy_score(y_test, rf_pred.round())
-tn, fp, fn, tp = confusion_matrix(y_test, rf_pred.round()).ravel()
-rf_specificity = tn / (tn+fp)
+print("true negative:",tn)
+print("false postive:",fp)
+print("false negative:",fn)
+print("true postive:",tp)
 print('specificity:', rf_specificity)
-
-
-rf_sensitivity  = tp / (fn + tp)
 print('sensitivity:', rf_sensitivity)
-
-from sklearn.metrics import roc_auc_score
-roc_rf=roc_auc_score(y_test, rf_pred.round())
 print('AUC: ',roc_rf)
+print('********************************')
+print('')
+
+'''
+for onesvm:
+Accuracy: 0.8169014084507042
+Precision: 0.3185840707964602
+Recall: 0.972972972972973
+specificity: 0.8020565552699229
+sensitivity: 0.972972972972973
+AUC:  0.8875147641214479
+'''
 
 
-#svm one class classification
-from sklearn.svm import OneClassSVM
-clf = OneClassSVM(gamma='auto').fit(x_train)
-c = clf.predict(x_test)
-s = clf.score_samples(x_test)
-#clf.score_samples(x_train)
-
-
-#use confusion matrix to get all 3
-from sklearn import metrics
-cnf_matrix = metrics.confusion_matrix(y_test, c.round())
-print('for SVM one class')
-print("Accuracy:",metrics.accuracy_score(y_test, c.round()))
-
-#print("Precision:",metrics.precision_score(y_test, c, average='micro'))
-#print("Recall:",metrics.recall_score(y_test, c, average='micro'))
-
-#tn, fp, fn, tp = confusion_matrix(y_test, c).ravel()
-#specificity = tn / (tn+fp)
-#print('specificity:', specificity)
-
-#sensitivity  = tp / (fn + tp)
-#print('sensitivity:', sensitivity)
-
-from sklearn.metrics import roc_auc_score
-roc_one=roc_auc_score(y_test, c.round())
-print('AUC: ',roc_one)
-
+'''
+for autoencoder
+Accuracy: 0.9976525821596244
+Precision: 1.0
+Recall: 0.972972972972973
+specificity: 1.0
+sensitivity: 0.972972972972973
+AUC:  0.9864864864864865
+'''
 
 #make a bar chart
 import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-
-
 
 barWidth = 0.2
 
@@ -328,25 +274,19 @@ bars2 = [specificity, s_specificity, rf_specificity]
 bars3 = [sensitivity, s_sensitivity, rf_sensitivity]
 bars4 = [roc_lr, roc_svm, roc_rf]
 
-
 #set position of bar on X axis 
 r1 = np.arange(len(bars1))
 r2 = [x + barWidth for x in r1]
 r3 = [x + barWidth for x in r2]
 r4 = [x + barWidth for x in r3]
 
-
 # Make the plot
 plt.bar(r1, bars1, color='#87CEFA', width=barWidth, edgecolor='white', label='var1')
 plt.bar(r2, bars2, color='#FFC1C1', width=barWidth, edgecolor='white', label='var2')
 plt.bar(r3, bars3, color='#FFE1FF', width=barWidth, edgecolor='white', label='var3')
-plt.bar(r4, bars4, color='#27408B', width=barWidth, edgecolor='white', label='var4')
-
-        
+plt.bar(r4, bars4, color='#27408B', width=barWidth, edgecolor='white', label='var4')     
 plt.xticks([r + barWidth for r in range(len(bars1))], ['Logistic Regression','SVM','Random Forrest'])
-
 plt.show
-
 
 metric = ['Accuracy', 'Specificity', 'Sensitivity', 'AUC']
 plt.legend(metric)
